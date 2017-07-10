d81e2a5e307f005a9cd5cdb2f1ad8751cb42d773
Alexey Nazarov <o4kan@ya.ru>
2017-07-10 16:42:02 +0300

    add Transaction Model and Mapper #em-40

---
+++ src/Mappers/TransactionMapper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Siqwell\DataScreen\Mappers;
+
+use Siqwell\DataScreen\Common\ObjectHydrator;
+use Siqwell\DataScreen\Models\Transaction;
+
+/**
+ * Class Mapper
+ * @package Siqwell\Eagle\Mappers
+ */
+class TransactionMapper extends AbstractMapper
+{
+    public function get()
+    {
+        $objectHydrator = new ObjectHydrator();
+
+        return $objectHydrator->hydrate(new Transaction(), $this->response['transaction']);
+    }
+}
---
+++ src/Models/Transaction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Siqwell\DataScreen\Models;
+
+class Transaction extends AbstractModel
+{
+    public $internal_id;
+    public $rightholder;
+    public $content;
+    public $channel;
+    /* ISO 3166-1 alpha-2 */
+    public $country;
+    public $price_without_vat;
+    public $share_per_unit;
+    public $ad_count;
+    public $preorder;
+}
