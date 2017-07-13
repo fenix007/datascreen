ab1638b08948f4e3f00383696e2213e570444ea7
Alexey Nazarov <o4kan@ya.ru>
2017-07-13 23:34:47 +0300

    move functionalit Api TestCase to FileHttpTrait

--- test/Siqwell/DataScreen/Tests/Api/TestCase.php
+++ test/Siqwell/DataScreen/Tests/Api/TestCase.php
@@ -5,10 +5,11 @@ namespace Siqwell\DataScreen\Tests\Api;
 use Siqwell\DataScreen\ApiToken;
 use Siqwell\DataScreen\Methods;
 use Siqwell\DataScreen\Tests\HttpClient\HttpClient;
-use Siqwell\DataScreen\HttpClient\Request;
 
 class TestCase extends \Siqwell\DataScreen\Tests\TestCase
 {
+    use FileHttpTrait;
+
     /** @var  \Siqwell\DataScreen\Tests\Api\Api */
     private $api;
 
@@ -18,25 +19,6 @@ class TestCase extends \Siqwell\DataScreen\Tests\TestCase
     public function __construct()
     {
         $this->api = new Api(new HttpClient(new ApiToken()), new Methods());
-    }
-
-    /**
-     * Get the expected request that will deliver a response
-     *
-     * @param $path
-     * @param  array   $parameters
-     * @param  string  $method
-     * @return \Siqwell\DataScreen\HttpClient\Request
-     */
-    protected function getRequest($path, $method = 'GET', array $parameters = []) : Request
-    {
-        $request = new Request(
-            $path,
-            $method,
-            $parameters
-        );
-
-        return $request;
     }
 
     /**
@@ -57,30 +39,6 @@ class TestCase extends \Siqwell\DataScreen\Tests\TestCase
     public function setRealApiClient()
     {
         $this->api->setClient(new \Siqwell\DataScreen\HttpClient\HttpClient(new ApiToken()), new Methods());
-    }
-
-    public function getResponseDataFromFile($method, $parameters = [])
-    {
-        $config = $this->createFakeConfig();
-        $path =  $config['base_uri'] . '/' . $method;
-
-        foreach ($parameters as $var => $val) {
-            $path = str_replace('{' . $var . '}', $val, $path);
-        }
-
-        if (!file_exists($path)) {
-            throw new \RuntimeException("Resource test file '$path' does not found");
-        }
-
-        if (!$response = @\json_decode(file_get_contents($path), true)) {
-             throw new \RuntimeException("Error to decode json file '$path'");
-        }
-
-        if (!isset($response['data'])) {
-            throw new \RuntimeException("Data field not found in response body file '$path'");
-        }
-
-        return $response['data'];
     }
 
     protected function getUrlWithoutQuery($url)
